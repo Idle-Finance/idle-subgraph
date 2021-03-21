ae20074293229acece61538d086e95c8617b2211
Asaf Silman <asafsilman@gmail.com>
2021-03-21 22:30:21 +0800

    Added handle transfer

--- src/mapping.ts
+++ src/mapping.ts
@@ -75,7 +75,6 @@ function handleBurn(event: TransferEvent): void {
   let user = getOrCreateUser(event.params.from, event.block)
   let token = getOrCreateToken(event.address)
 
-  // would be faster to create new instance, and overwrite existing userToken
   let userToken = getOrCreateUserToken(user, token)
 
   userToken.balance = userToken.balance - event.params.value;
@@ -83,7 +82,18 @@ function handleBurn(event: TransferEvent): void {
 }
 
 function handleTokenTransfer(event: TransferEvent): void {
+  let token = getOrCreateToken(event.address)
+  let userFrom = getOrCreateUser(event.params.from, event.block)
+  let userTo = getOrCreateUser(event.params.to, event.block)
 
+  let userTokenFrom = getOrCreateUserToken(userFrom, token)
+  let userTokenTo = getOrCreateUserToken(userTo, token)
+
+  userTokenFrom.balance = userTokenFrom - event.params.value;
+  userTokenTo.balance = userTokenTo + event.params.value;
+
+  userTokenFrom.save()  
+  userTokenTo.save()  
 }
 
 export function handleTransfer(event: TransferEvent): void {
@@ -100,48 +110,3 @@ export function handleTransfer(event: TransferEvent): void {
     handleTokenTransfer(event)
   }
 }
-
-// export function handleAdminChanged(event: AdminChanged): void {
-//   // Entities can be loaded from the store using a string ID; this ID
-//   // needs to be unique across all entities of the same type
-//   let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-//   // Entities only exist after they have been saved to the store;
-//   // `null` checks allow to create entities on demand
-//   if (entity == null) {
-//     entity = new ExampleEntity(event.transaction.from.toHex())
-
-//     // Entity fields can be set using simple assignments
-//     entity.count = BigInt.fromI32(0)
-//   }
-
-//   // BigInt and BigDecimal math are supported
-//   entity.count = entity.count + BigInt.fromI32(1)
-
-//   // Entity fields can be set based on event parameters
-//   entity.previousAdmin = event.params.previousAdmin
-//   entity.newAdmin = event.params.newAdmin
-
-//   // Entities can be written to the store with `.save()`
-//   entity.save()
-
-//   // Note: If a handler doesn't require existing field values, it is faster
-//   // _not_ to load the entity from the store. Instead, create it fresh with
-//   // `new Entity(...)`, set the fields that should be updated and save the
-//   // entity back to the store. Fields that were not set or unset remain
-//   // unchanged, allowing for partial updates to be applied.
-
-//   // It is also possible to access smart contracts from mappings. For
-//   // example, the contract that has emitted the event can be connected to
-//   // with:
-//   //
-//   // let contract = Contract.bind(event.address)
-//   //
-//   // The following functions can then be called on this contract to access
-//   // state variables and other data:
-//   //
-//   // - contract.implementation(...)
-//   // - contract.admin(...)
-// }
-
-// export function handleUpgraded(event: Upgraded): void {}
