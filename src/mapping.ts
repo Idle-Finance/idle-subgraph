32c34c01e54e81c3c1cf3305db391683229a38b1
Asaf Silman <asafsilman@gmail.com>
2021-03-21 20:32:52 +0800

    Added handers for mints and burns

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,8 +1,105 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, Address, ethereum, store, log } from "@graphprotocol/graph-ts"
+
+import { ADDRESS_ZERO, TOKEN_MAPPING } from "./helpers"
+
 import {
-  Transfer
+  Transfer as TransferEvent
 } from "../generated/idleDAIBestYield/IdleTokenGovernance"
-import { TransferEntity } from "../generated/schema"
+import { User, Token, UserToken } from "../generated/schema"
+
+function getOrCreateUser(userAddress: Address, block: ethereum.Block): User {
+  let userId = userAddress.toHex()
+  let user = User.load(userId)
+  if (user==null) {
+    user = new User(userId)
+
+    user.firstInteractionTimestamp = block.timestamp
+    user.save()
+  }
+  return user as User
+}
+
+function getOrCreateToken(tokenAddress: Address): Token {
+  let tokenId = tokenAddress.toHex()
+  let token = Token.load(tokenId)
+  if (token==null) {
+    token = new Token(tokenId)
+
+    token.address = tokenAddress
+    
+    let tokenName = "Default"
+    if (TOKEN_MAPPING.has(tokenAddress.toHexString())) {
+      tokenName = TOKEN_MAPPING.get(tokenAddress.toHexString())
+    }
+    else {
+      log.warning("Token {} does not exist in mapping", [tokenAddress.toHexString()])
+    }
+    
+    token.name = tokenName
+
+    token.save()
+  }
+  return token as Token
+}
+
+function getOrCreateUserToken(user: User, token: Token): UserToken {
+  let userTokenId = user.id.toString()
+    .concat("-")
+    .concat(token.id.toString())
+
+  let userToken = UserToken.load(userTokenId)
+  if (userToken == null) {
+    userToken = new UserToken(userTokenId)
+
+    userToken.user = user.id
+    userToken.token = token.id
+    userToken.balance = BigInt.fromI32(0)
+
+    userToken.save()
+  }
+
+  return userToken as UserToken
+}
+
+function handleMint(event: TransferEvent): void {
+  let user = getOrCreateUser(event.params.to, event.block)
+  let token = getOrCreateToken(event.address)
+
+  let userToken = getOrCreateUserToken(user, token)
+
+  userToken.balance = userToken.balance + event.params.value;
+  userToken.save()
+}
+
+function handleBurn(event: TransferEvent): void {
+  let user = getOrCreateUser(event.params.from, event.block)
+  let token = getOrCreateToken(event.address)
+
+  // would be faster to create new instance, and overwrite existing userToken
+  let userToken = getOrCreateUserToken(user, token)
+
+  userToken.balance = userToken.balance - event.params.value;
+  userToken.save()
+}
+
+function handleTokenTransfer(event: TransferEvent): void {
+
+}
+
+export function handleTransfer(event: TransferEvent): void {
+  if (event.params.from.toHexString() == ADDRESS_ZERO) {
+    log.info("Mint Detected", [])
+    handleMint(event)
+  }
+  else if (event.params.to.toHexString() == ADDRESS_ZERO) {
+    log.info("Burn Detected", [])
+    handleBurn(event)
+  }
+  else {
+    log.info("Transfer Detected", [])
+    handleTokenTransfer(event)
+  }
+}
 
 // export function handleAdminChanged(event: AdminChanged): void {
 //   // Entities can be loaded from the store using a string ID; this ID
@@ -48,13 +145,3 @@ import { TransferEntity } from "../generated/schema"
 // }
 
 // export function handleUpgraded(event: Upgraded): void {}
-
-export function handleTransfer(event: Transfer): void {
-  let entity = new TransferEntity(event.transaction.from.toHex())
-
-  entity.to = event.params.to;
-  entity.from = event.params.from;
-  entity.value = event.params.value;
-
-  entity.save()
-}
